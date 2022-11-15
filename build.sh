088304eb572dd94c88395ff4390c537a3b77ed9f
MouseSun846 <3026098675@qq.com>
2022-11-15 15:52:30 +0800

    初始化git设置

---
+++ build.sh
@@ -0,0 +1,3 @@
+git config --global user.name "MouseSun846"
+git config --global user.email "3026098675@qq.com"
+git config --global remote.origin.url "https://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@github.com/MouseSun846/CppKnowledgeNote.git"
