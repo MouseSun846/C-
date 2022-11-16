540f4f93fa1cdace08a9af33274f946af93c4297
MouseSun846 <3026098675@qq.com>
2022-11-16 12:44:02 +0800

    删除token

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 git config --global user.name "MouseSun846"
 git config --global user.email "3026098675@qq.com"
-git remote add origin "https://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@github.com/MouseSun846/CppKnowledgeNote.git"
